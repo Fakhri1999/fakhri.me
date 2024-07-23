0027ce92b069dd3dd273e2bf7d814691b7b0e3f1
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2024-07-23 20:49:16 +0700

    feat: change axios to fetch in api route

--- pages/api/form.js
+++ pages/api/form.js
@@ -1,9 +1,16 @@
-import axios from 'axios';
 import moment from 'moment';
 
 const handler = async (req, res) => {
   if (req.method != 'POST') {
-    return res.status(404).json({ message: 'Not found' });
+    return reFs.status(404).json({ message: 'Not found' });
+  }
+
+  if (!process.env.DISCORD_WEBHOOK_URL) {
+    console.error('[/api/form] error: Discord webhook URL is missing');
+    return res.status(500).json({
+      message:
+        'Missing configuration. Please contact me to fix this issue. Thank you',
+    });
   }
 
   const { name, contact, details } = req.body;
@@ -18,17 +25,21 @@ const handler = async (req, res) => {
       },
     ],
   };
+
   try {
-    const result = await axios({
-      url: process.env.DISCORD_WEBHOOK_URL,
+    await fetch(process.env.DISCORD_WEBHOOK_URL, {
       method: 'POST',
-      data: apiData,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(apiData),
     });
     return res.status(200).json({
       message:
         'Your form has been saved successfully. Thank you for contacting me',
     });
   } catch (error) {
+    console.error('[/api/form] error: ', error);
     return res.status(500).json({
       message:
         'Something went wrong. Please contact me via other method or try again later',
